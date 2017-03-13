4d61a201a5185e94ac23c220bbb5c05b934d0f5c
Jon Horn <jhorn85@gmail.com>
2017-03-12 21:07:16 -0400

    looping input string to scoring array

--- scoreTheRace/scoreTheRace/hw8.cpp
+++ scoreTheRace/scoreTheRace/hw8.cpp
@@ -11,20 +11,29 @@ using namespace std;
 int main() {
     string input;
     
-    while (true) {
+    int charCounter[91] = {0};
+    int x = 0;
+    while (x < 1) {
+        bool validInput = true;
         cout << "Enter the race" << endl;
         cin >> input;
         transform(input.begin(), input.end(),input.begin(), ::toupper);
-        char stringArray[input.length()];
         
-        for (int i = 0; i < length; i++) {
-            stringArray[i] = input[i];
-            cout << stringArray[i];
+        for (int i = 0; i < input.length(); i++) {
+            //offset of 65
+            // This uses the ascii value as index to a letter map and element value as counter
+            int asciiVal = (int)input[i];
+            charCounter[asciiVal] += (i + 1);
         }
-        if (input == "done") {
-            cout << "Done" << endl;
-            break;
-        }
+        x++;
+//        if (input == "") {
+//            cout << "Done" << endl;
+//            break;
+//        }
+        
+    }
+    for (int i = 65; i < 91; i++) {
+        cout << charCounter[i];
     }
     return 0;
 }
