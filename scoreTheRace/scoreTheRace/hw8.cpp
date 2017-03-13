1ab5036fa35aca6f9ac170dd88394c4a13d16eb3
Jon Horn <jhorn85@gmail.com>
2017-03-13 16:58:22 -0400

    more refactoring and added some commments

--- scoreTheRace/scoreTheRace/hw8.cpp
+++ scoreTheRace/scoreTheRace/hw8.cpp
@@ -8,13 +8,31 @@
 #include <iostream>
 using namespace std;
 
-bool greaterThan(int a, int b) {
-    return a < b;
-}
-
-bool lessThan(int a, int b) {
-    return a < b;
-}
+//bool greaterThan(int a, int b) {
+//    return a < b;
+//}
+//
+//bool lessThan(int a, int b) {
+//    return a < b;
+//}
+//
+//typedef int (*FUNC)(int, int);
+//
+//int findDifference(int array[26][2], int column,  *FUNC) {
+//    int min = 0;
+//    
+//    // find the first non zero
+//    if (array[0][column] == 0)
+//        while (array[min][0] == 0)
+//            min++;
+//    
+//    // find smallest non zero
+//    for (int i = 0; i < 26; i++)
+//        if (array[i][column] != 0 && *func(array[i][column], array[min][column]))
+//            min = i;
+//    
+//    return min;
+//}
 
 // return smallest team size != 0
 // column represents whether we are testing the team score or number of team members
@@ -45,17 +63,18 @@ int findLargest(int array[26][2]) {
     return max;
 }
 
-//typedef int (*FUNC)(int);
-//int compareValues(FUNC, int a, int b);
-
 int main() {
     string input;
     
     // array to track the score each runner generates
-    bool validInput = true, teamSizesEqual = true;
+    bool validInput = true;
     
     while (input != "DONE") {
+        
+        // scoringTable = { [number of runners per team], [team score] }
+        
         int scoringTable[26][2] = {0}, asciiVal = 0, numOfTeams = 0, minTeamSize = 0, maxTeamSize = 0;
+        
         cout << "Enter the race" << endl;
         cin >> input;
         transform(input.begin(), input.end(),input.begin(), ::toupper);
@@ -67,11 +86,14 @@ int main() {
             //convert each char to ascii
             asciiVal = (int)input[i] - 65;
             
+            // increment number of runners
             scoringTable[asciiVal][0] += 1;
+            
+            // add score to team based on position in race
             scoringTable[asciiVal][1] += (i + 1);
         }
         
-        // counts the number of teammates per race TEST CODE
+        // counts the number of teammates per race
         for (int i = 0; i < 26; i++)
             if (scoringTable[i][0] != 0)
                 numOfTeams++;
@@ -79,12 +101,11 @@ int main() {
         int indexOfSmallestTeam = findSmallest(scoringTable, 0);
         int indexOfLargestTeam = findLargest(scoringTable);
 
-        // test if both teams are equal size using min and max
+        // test if both teams are equal size
         minTeamSize = scoringTable[indexOfSmallestTeam][0];
         maxTeamSize = scoringTable[indexOfLargestTeam][0];
-        teamSizesEqual = (maxTeamSize == minTeamSize);
         
-        if (!teamSizesEqual) {
+        if (!(maxTeamSize == minTeamSize)) {
             validInput = false;
             cout << "Teams must be of equal size" << endl;
         } else {
@@ -92,8 +113,8 @@ int main() {
             cout << "Each team has " << maxTeamSize << " runners." << endl;
             cout << "Team       Score" << endl;
             
-            // test if letter was scored and print char version
             for (int i = 0; i < 26; i++) {
+                // if team ran in race, convert to char and print their team name
                 if (scoringTable[i][1] != 0) {
                     // use the index of array to get char ascii code
                     char team = (char)i + 65;
@@ -101,7 +122,6 @@ int main() {
                     cout << team << "          " << score << endl;
                 }
             }
-            
             char winningTeam = (char)findSmallest(scoringTable, 1) + 65;
             cout << "The winning team is team " << winningTeam << endl;
         }
