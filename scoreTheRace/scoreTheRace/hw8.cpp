aff18deba06afd05332aa341c39f78bfbd743481
Jon Horn <jhorn85@gmail.com>
2017-03-12 22:24:44 -0400

    now tracking number of teams and checking if number of team members are equal across all teams

--- scoreTheRace/scoreTheRace/hw8.cpp
+++ scoreTheRace/scoreTheRace/hw8.cpp
@@ -8,32 +8,42 @@
 #include <iostream>
 using namespace std;
 
+// return smallest value != 0
+bool minValueNotZero(int a, int b) {
+    return a != 0 && a < b;
+}
+
 int main() {
     string input;
     
-    int charCounter[91] = {0};
-    int x = 0;
-    while (x < 1) {
-        bool validInput = true;
+    // array to track the score each runner generates
+    int scoreKeeper[91] = {0}, teamSize[26], asciiVal = 0;
+    bool validInput = true, teamSizesEqual = true;
+    
+    while (validInput || input != "DONE") {
         cout << "Enter the race" << endl;
         cin >> input;
         transform(input.begin(), input.end(),input.begin(), ::toupper);
+        if (input == "DONE")
+            break;
         
         for (int i = 0; i < input.length(); i++) {
-            //offset of 65
-            // This uses the ascii value as index to a letter map and element value as counter
-            int asciiVal = (int)input[i];
-            charCounter[asciiVal] += (i + 1);
-        }
-        x++;
-//        if (input == "") {
-//            cout << "Done" << endl;
-//            break;
-//        }
+            asciiVal = (int)input[i];
+            teamSize[asciiVal - 65]++;
         
-    }
-    for (int i = 65; i < 91; i++) {
-        cout << charCounter[i];
+            // This uses the ascii value as index and weighted positioning as score for each runner
+            scoreKeeper[asciiVal] += (i + 1);
+        }
+        
+        // check if teams are of equal size
+        teamSizesEqual =
+            (*max_element(teamSize, teamSize + 26)) ==
+            (*min_element(teamSize, teamSize + 26, minValueNotZero));
+        cout << teamSizesEqual;
+        if (!teamSizesEqual) {
+            validInput = false;
+            cout << "Teams must be of equal size" << endl;
+        }
     }
     return 0;
 }
