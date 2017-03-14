bac41cc5734145b9b62979b03d154a1ea787e9fa
Jon Horn <jhorn85@gmail.com>
2017-03-14 19:16:36 -0400

    added white and black queen template

--- chessBoard/chessBoard/chessBoard.cpp
+++ chessBoard/chessBoard/chessBoard.cpp
@@ -26,24 +26,83 @@ void backtrack(int &col){
         //set stop point
         exit(0);
     }
-    
 }
+
 void print(int board[], int numOfColumns, int numOfSolutions) {
-    int i = 0, j = 0;
-    cout << "Solution number " << numOfSolutions << " is: " << endl;
-    for(int i = 0; i < numOfColumns; i++)
-        cout << board[i] << " ";
-    cout << endl << endl;
-    for (i = 0; i < numOfColumns; i++) {
-        for (j = 0; j < numOfColumns; j++) {
-            if (i == board[j])
-                cout << "1 ";
-            else cout << "0 ";
+    const string black = "\u2588";
+    typedef string box[5][7];
+    box blackBox, whiteBox, *chessBoard[8][8], whiteQueen, blackQueen;
+    int i = 0, j = 0, k = 0, l = 0;
+    
+    for (i = 0; i < 5; ++i){        //for each row
+        for (j = 0; j < 7; ++j) {   //for each column
+            whiteBox[i][j] = ' ';         //fill white (space char)
+            whiteQueen[i][j] = ' ';       //fill white queen initially all white
+            blackBox[i][j] = black;   //fill black
+            blackQueen[i][j] = black;   //fill black queen initally all black
         }
-        cout << endl;
     }
+    
+    whiteQueen[1][1] = black;
+    whiteQueen[1][3] = black;
+    whiteQueen[1][5] = black;
+    for (int x = 1; x < 7; x++)
+        for (int y = 2; y < 4; y++)
+            whiteQueen[x][y] = black;
+    
+    blackQueen[1][1] = " ";
+    blackQueen[1][3] = " ";
+    blackQueen[1][5] = " ";
+    for (int x = 1; x < 7; x++)
+        for (int y = 2; y < 4; y++)
+            blackQueen[x][y] = " ";
+    
+    //fill board with pointers to blackBox and whiteBox in alternate positions
+    for (i = 0; i < 8; ++i){        //for each row
+        for (j = 0; j < 8; ++j){    //for each column
+            if ((i + j) % 2 == 0) chessBoard[i][j] = &whiteBox;
+            else chessBoard[i][j] = &blackBox;
+        }
+    }
+    
+    // print the board via the pointers in array board
+    // first print upper border
+    cout<<" ";
+    for (i = 0; i < 7 * 8; i++)
+        cout << '_';
     cout << endl;
+    
+    // now print the board
+    for (i = 0; i < 8; ++i){                //for each board row
+        for (k = 0; k < 5; ++k) {           //for each box row
+            cout<<" "<< string("\u2503"); //print left border
+            for (j = 0; j < 8; ++j) {        //for each board column
+                for (l = 0; l < 7; ++l) {    //for each box column
+                    cout << (*chessBoard[i][j])[k][l]; //k is box row, l is box column
+                }
+            }
+            cout<<string("\u2503")<<endl; // at end of line print bar and then newline
+        }
+    }
+    cout<<" ";
+    for(i=0;i<7*8;i++)
+        cout<<string("\u2501");
+    cout<<endl;
 }
+//    cout << "Solution number " << numOfSolutions << " is: " << endl;
+//    for (int i = 0; i < numOfColumns; i++)
+//        cout << board[i] << " ";
+//    cout << endl << endl;
+//    for (i = 0; i < numOfColumns; i++) {
+//        for (j = 0; j < numOfColumns; j++) {
+//            if (i == board[j])
+//                cout << "1 ";
+//            else cout << "0 ";
+//        }
+//        cout << endl;
+//    }
+//    cout << endl;
+
 int main() {
     
     int boardSize = 8, numOfSolutions = 0;
@@ -82,5 +141,7 @@ int main() {
         print(board, boardSize, ++numOfSolutions) ;/*print section*/
         from_backtrack = true;
         backtrack(column);
+        cout << "Press ENTER to continue...";
+        cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
     }
 }
