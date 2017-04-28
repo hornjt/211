046b5f23810610039158a07a5193ec30a83043e4
Jon Horn <jhorn85@gmail.com>
2017-04-27 21:35:25 -0400

    found last point on shortest route

--- shortestPath/shortestPath/main.cpp
+++ shortestPath/shortestPath/main.cpp
@@ -12,6 +12,13 @@ using namespace std;
 const int rows = 5;
 const int cols = 6;
 
+int findShortest(int array[5][6], int row, int col) {
+    int above = array[(row + 4) % 5][col];
+    int left = array[row][col];
+    int below = array[(row + 6) % 5][col];
+    return min(above, min(left, below));
+}
+
 int main(){
     int weight[rows][cols]={
         {3,4,1,2,8,6},
@@ -20,8 +27,6 @@ int main(){
         {8,4,1,3,2,6},
         {3,7,2,8,6,4}
     };
-    
-//    cout << (1 + 5 ) % 5 << endl;
     
     int memo_cost[rows][cols]={0};
     
@@ -34,32 +39,29 @@ int main(){
 //    fill in rest colum of memo_cost[][]
     for (int i = 0; i < cols - 1; i++) {
         for (int j = 0; j < rows; j++) {
-            int above = memo_cost[(j + 4) % 5][i];
-            int left = memo_cost[j][i];
-            int below = memo_cost[(j + 6) % 5][i];
+            int shortest = findShortest(memo_cost, j, i);
             
-//            if (memo_cost[j][i + 1] == 0)
-            memo_cost[j][i + 1] = weight[j][i + 1] + min(above, min(left, below));
-//            else memo_cost[j][i + 1] = memo_cost[j][i + 1] + min(above, min(left, below));
-    
-            int stop = 0;
-            
-//            memo_cost[j][i + 1] =
-            
-//            cout << "Row " << i << " column " << j + 1 << " has neighbors of " << above << left << below << endl;
-        
+            // now find the smallest of them
+            memo_cost[j][i + 1] = weight[j][i + 1] + shortest;
         }
     }
     
-    // now find the smallest of them
+    
     
     int route[cols]={-1};
-//    route[cols-1]=____; //find final step route.
     
-    for(int c=cols-2;c>=0;c--){// go backward to find route
-        //compare 3 square and find min
-        
-//        route[c]=___;
+    int min = memo_cost[0][cols - 1];
+    
+    for (int i = 1; i < rows; i++) {
+        if (memo_cost[i][cols - 1] < memo_cost[i - 1][cols - 1])
+            min = i;
+    }
+    
+    route[cols-1]= memo_cost[min][cols - 1]; //find final step route.
+    
+    
+    for(int c=cols-1;c>=0;c--){// go backward to find route
+        route[c]= findShortest(memo_cost, );
     }
     
     //print route
